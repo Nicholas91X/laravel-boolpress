eb6a5629ba063dbc5908a724e682fa4cacd684ad
Nicholas <romitinicholas@gmail.com>
2022-02-25 00:33:29 +0100

    vue router

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <router-view></router-view>
+  </main>
+</template>
+
+<script>
+export default {
+    name: "Main",
+    data() {
+        return {
+            posts: []
+        }
+    },
+    created() {
+        axios.get('/api/posts')
+        .then ( (response) => {
+            this.posts = response.data;
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
